a77852daec73e13fa6037e88a808fd1b27ca091e
Mattias Petter Johansson <mpj@mpj.me>
2017-09-16 11:07:28 +0200

    added file logging

--- supersniff.js
+++ supersniff.js
@@ -1,7 +1,17 @@
+const util = require('util')
+const fs = require('fs')
+const writeFile = util.promisify(require('fs').writeFile)
+
 const fn = (tagName, val) => {
   console.log(`[${tagName}]`, val)
   return val
 }
 const api = fn.bind(null, 'SNIFF')
 api.tag = tagName => fn.bind(null, tagName)
+api.file = filename => val =>
+  writeFile(filename, JSON.stringify(val, null, 2)).then(() => {
+    console.log(`supersniff wrote to ${filename}`)
+    return val
+  })
+
 module.exports = api
